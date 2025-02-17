6a090e63efd44a5fe8807a7ebffa43e6b4f5a388
imzzz01 <cyon2z2j@students.codeinstitute.net>
2025-02-17 21:09:54 +0000

    Fix: errors that were identified in JS lint!

--- assets/script.js
+++ assets/script.js
@@ -14,7 +14,7 @@ $("#search-btn").click(function () {
           $("#back-to-recommendations-btn").show();
         //Show back to recommendations button
          $("#movie-details").hide(); //Hide movie details
-        $('#movie-results').show(); //Show movie results
+        $("#movie-results").show(); //Show movie results
 }
 });
 //Back to recommendations button click event
@@ -52,264 +52,291 @@ $("#back-to-recommendations-btn").click(function (){
 
 
     ];
-
-   
-    const carouselInner = $('#recommended-carousel-inner');
+    const carouselInner = $("#recommended-carousel-inner");
     carouselInner.empty(); // Clear previous recommendations
 
     // Loop through recommended movies and fetch data from OMDB Api
-    recommendedMovies.forEach((movie, index) => {
+    recommendedMovies.forEach(function(movie, index) {
         $.ajax({
-            url:`https://www.omdbapi.com/`,
-            method: 'GET',
-            data:{
-                 i: movie.imdbID,
-                 apikey: '2fee485b'
+           data: {
+                apikey: "2fee485b",
+                i: movie.imdbID
             },
-    
+            error: function(){
+       console.log("Error fetching movie data");
+    },
+            method: "GET",
             success: function(data){
-                
-                let poster = data.Poster !== "N/A" ? data.Poster : './assets/images/default.jpg';
-                    
-
-                    const activeClass = index === 0 ? "active" : ""; // First item should be active
-                    const movieItem =`
+               let poster = (
+               data.Poster !== "N/A"
+               ? data.Poster
+               : "./assets/images/default.jpg"
+               );
+               const activeClass = (
+               index === 0
+               ? "active"
+               : ""
+                   );
+   // First item should be active
+             const movieItem =`
                     <div class="carousel-item ${activeClass}">
-                    <img src="${poster}" class="d-block w-100" alt="${movie.title}" style="max-width: 300px; margin: auto;">
+                    <img
+                    src="${poster}"
+                    class="d-block w-100"
+                    alt="${movie.title}
+                    style="max-width: 300px; margin: auto;">
                      <div class="carousel-caption d-none d-md-block">
                    <h5>${movie.title}</h5>
-                   <button class="btn btn-outline-light" onclick="getMovieDetails('${movie.imdbID}')">View Details</button>
+                   <button
+                   class="btn btn-outline-light"
+                onclick="getMovieDetails('${movie.imdbID}')
+                   ">View Details</button>
 
          </div>
          </div>
          `;
-         carouselInner.append(movieItem);
 
-            },      
-    error: function(){
-       console.log("Error fetching movie data");
-    }
+       carouselInner.append(movieItem);
+
+  },
+            url:"https://www.omdbapi.com/"
         });
     });
-}
 
-
+  }
     // Dark-mode toggle functionality
     $(document).ready(function() {
 
         if(localStorage.getItem("dark-mode")==="enabled"){
             $("body").addClass("dark-mode");
             $("#dark-mode-toggle").text("Light Mode");
-    
         } else {
             $("#dark-mode-toggle").text("dark-mode");
-        } 
-    
+        }
         $("#dark-mode-toggle").click(function() {
         $("body").toggleClass("dark-mode");
-    
+
         if ($("body").hasClass("dark-mode")) {
             $("#dark-mode-toggle").text("Light Mode");
             localStorage.setItem("dark-mode","enabled");
-    
+
         } else {
             $("#dark-mode-toggle").text("dark-mode");
             localStorage.removeItem("dark-mode");
         }
     });
     });
-    
 
-updateFavoriteCount(); //Update favourite count on page load 
+
+updateFavoriteCount(); //Update favourite count on page load
 
 // Function to update favourite count
     function updateFavoriteCount() {
-    let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-    $('#favorites-count').text(favorites.length);
+    let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+    $("#favorites-count").text(favorites.length);
 }
 
 
 // Function to search for movies/dramas using OMDB API
  function searchMovie(query) {
     $.ajax({
-        url:`https://www.omdbapi.com/`,
-        method: 'GET',
         data:{
-             s: query,
-             apikey: '2fee485b'
+            apikey: "2fee485b",
+            s: query
         },
+        error: function(){
+$("#movie-results").html("<p>Error fetching data. Please try again later.</p>");
+},
+        method: "GET",
 
         success: function(data){
-            console.log('Movie Search Response:', data);
+            console.log("Movie Search Response:", data);
             if(data.Response === "True") {
                 displayMovies(data.Search);
             } else {
-        $('#movie-results').html('<p>No movies found. Please try again.</p>');
+        $("#movie-results").html("<p>No movies found. Please try again.</p>");
             }
-        
             },
-error: function(){
-    $('#movie-results').html('<p>Error fetching data. Please try again later.</p>');
-}
+
+
+        url:"https://www.omdbapi.com/"
     });
-};
+}
 
 // Function to display  searched movies/dramas on screen
 
   function displayMovies(movies) {
-    $('#movie-results').empty();
-    movies.forEach(movie => {
-        const poster = movie.Poster!== "N/A" ? movie.Poster : './assets/images/default-image.jpg';
+    $("#movie-results").empty();
+    movies.forEach(function(movie) {
+     const poster = (
+        movie.Poster !== "N/A"
+           ? movie.Poster
+           : "./assets/images/default-image.jpg"
+     );
 
         const movieCard = `
         <div class="col-md-3 col-sm-6 col-12 movie-card" data-aos="zoom-in">
-        <img src="${poster}" alt="${movie.Title}" class="clickable-poster" data-imdbID="${movie.imdbID}" style="cursor: pointer;">
+       <img
+       src="${poster}" alt="${movie.Title}"
+       class="clickable-poster" data-imdbID="${movie.imdbID}"
+       style="cursor: pointer;">
         <h3>${movie.Title}</h3>
         <p>${movie.Year}</p>
-        <button class="btn btn-outline-primary w-100" onclick="getMovieDetails('${movie.imdbID}')">View Details</button>
+     <button
+     class="btn btn-outline-primary
+     w-100" onclick="getMovieDetails('${movie.imdbID}')">
+    View Details</button>
         </div>
         `;
 
-        $('#movie-results').append(movieCard);
+        $("#movie-results").append(movieCard);
 
     });
 
-    $('.clickable-poster').click(function() {
-        const imdbID = $(this).data('imdbid');
+    $(".clickable-poster").click(function(event) {
+        const imdbID = $(event.currentTarget).data("imdbid");
         getMovieDetails(imdbID);
     });
 }
 
 // Function to fetch movie details from OMDB API
   function getMovieDetails(imdbID) {
- 
    $.ajax({
-    url:`https://www.omdbapi.com/`,
-    method: 'GET',
-    data:{
-         i: imdbID,
-         apikey: '2fee485b'
+     data:{
+         apikey: "2fee485b",
+         i: imdbID
     },
+       error: function(){
+$("#movie-results").html
+("<p>Error fetching movie details. Please try again later.</p>");
+},
+       method: "GET",
 
     success: function(data){
-        console.log('Movie Details Response:', data);
+        console.log("Movie Details Response:", data);
         if(data.Response === "True"){
         showMovieDetails(data);
         fetchTrailer(data.Title);
     } else {
-        $('#movie-details').html('<p>Movie details not found.</p>');
-    
+        $("#movie-details").html("<p>Movie details not found.</p>");
     }
-    
         },
-    
-    
-error: function(){
-    $('#movie-results').html('<p>Error fetching movie details. Please try again later.</p>');
-}
+     url:"https://www.omdbapi.com/"
     });
 
 }
 // Function to fetch movie trailers from Youtube API
   function fetchTrailer(movieTitle) {
     $.ajax({
-        url:`https://www.googleapis.com/youtube/v3/search`,
-        method: 'GET',
         data:{
-             part: 'snippet',
+             key: "AIzaSyDnw9QhSAomQ46lydMI_JDp-a8VVzFThV4",
+             part: "snippet",
              q: `${movieTitle} trailer`,
-             type: 'video',
-             key: 'AIzaSyDnw9QhSAomQ46lydMI_JDp-a8VVzFThV4'
+             type: "video"
         },
-    
+         error: function(){
+$("#movie-details").append
+("<p>Error fetching trailer. Please try again later.</p>");
+        },
+        method: "GET",
         success: function(response){
-            
             const videoId = response.items[0]?.id?.videoId;
             if (videoId) {
                 const trailerEmbed = `
                 <h4>Watch the Trailer:</h4>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" 
-                frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+           <iframe width="560"
+           height="315"
+           src="https://www.youtube.com/embed/${videoId}"
+         frameborder="0" allow="accelerometer;
+         autoplay; encrypted-media; gyroscope; picture-in-picture"
                allowfullscreen></iframe>
             `;
-            $('#movie-details').append(trailerEmbed);
+            $("#movie-details").append(trailerEmbed);
             }else{
-                $('#movie-details').append('<p>No trailer found. </p>');
+                $("#movie-details").append("<p>No trailer found. </p>");
             }
         },
 
-        error: function(){
-            $('#movie-details').append('<p>Error fetching trailer. Please try again later.</p>');
-        }
+    url:`https://www.googleapis.com/youtube/v3/search`
 
         });
     }
 
 // Function to show movie details when ypu press view details
   function showMovieDetails(movie) {
-   
-    const poster = movie.Poster !== "N/A" ? movie.Poster : './assets/images/defualt-image.jpg';
-    $('#movie-details').html(`
-        <img src="${poster}"alt="${movie.Title}" style="width: 300px; border-radius: 8px;">
+
+    const poster = (
+          movie.Poster !== "N/A"
+             ? movie.Poster
+             : "./assets/images/defualt-image.jpg"
+        );
+    $("#movie-details").html(`
+       <img
+        src="${poster}"alt="${movie.Title}"
+        style="width: 300px; border-radius: 8px;">
         <h2>${movie.Title} (${movie.Year})</h2>
         <p><strong>Genre:</strong>${movie.Genre}</p>
         <p><strong>Rating:</strong>${movie.imdbRating}</p>
         <p><strong>Plot:</strong>${movie.Plot}</p>
         <button id="favorite-button" class="btn btn-outline-secondary">
-        ${isFavorite(movie.imdbID) ? 'Remove from Favorites': 'Add to Favorites'} </button>
+${(
+      isFavorite(movie.imdbID)
+          ? "Remove from Favorites"
+          : "Add to Favorites"
+)}
+          </button>
    <br><br>
-        <button class="btn btn-primary" onclick="backToMovies()">Back to Movies</button>     
+ <button class="btn btn-primary"
+ onclick="backToMovies()">Back to Movies</button>
         </div>
 
     `);
 
-    $('#favorite-button').click(function() {
+    $("#favorite-button").click(function() {
         toggleFavorite(movie.imdbID, movie.Title);
     });
-        
-        $('#movie-results').hide();
-        $('#movie-details').show();
-       
-    
+        $("#movie-results").hide();
+        $("#movie-details").show();
     }
     function backToMovies() {
-        $('#movie-results').show();
-        $('#movie-details').empty();
+        $("#movie-results").show();
+        $("#movie-details").empty();
     }
-    
+
 // Function to check if a movie/drama is in favourites
 
     function toggleFavorite(imdbID, title) {
-    let favorites = JSON.parse(localStorage.getItem('favorites')) ||[];
-    const movieExists = favorites.some(movie => movie.imdbID === imdbID);
+    let favorites = JSON.parse(localStorage.getItem("favorites")) ||[];
+    const movieExists = favorites.some((movie) => movie.imdbID === imdbID);
 
     if(movieExists) {
-        favorites = favorites.filter(movie => movie.imdbID !== imdbID);
-        Swal.fire('Removed!',`${title} has been removed from your favorites.`,'info');
-        // When you remove from favourites - a pop up should display on the bottom of the screen.
+        favorites = favorites.filter((movie) => movie.imdbID !== imdbID);
+Swal.fire("Removed!",`${title} has been removed from your favorites.`,"info");
+   // When you remove from favourites
+   // a pop up should display on the bottom of the screen.
     }else {
-        
+
         favorites.push({imdbID, title});
-        Swal.fire('Added!',`${title} has been added to your favorites.`, 'success');
+   Swal.fire("Added!",`${title} has been added to your favorites.`, "success");
     }
-    localStorage.setItem('favorites', JSON.stringify(favorites));
+    localStorage.setItem("favorites", JSON.stringify(favorites));
 
     updateFavoriteCount();
     updateFavoriteButton(imdbID);
-   
+
 }
 
 function updateFavoriteButton(imdbID) {
-    const favoriteButton = $('#favorite-button');
+    const favoriteButton = $("#favorite-button");
     if(isFavorite(imdbID)) {
-        button.text('Remove from Favorites');
+        button.text("Remove from Favorites");
     } else {
-        button.text('Add to Favorites');
-       
+        button.text("Add to Favorites");
+
     }
 }
 
   function isFavorite(imdbID) {
-         let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-         return favorites.some(movie => movie.imdbID === imdbID);
-}
+         let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+         return favorites.some((movie) => movie.imdbID === imdbID);
+  }
